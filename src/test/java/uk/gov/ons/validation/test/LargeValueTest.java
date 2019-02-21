92f24d303a44c09e53bb24ab622e057a371e2a4e
dllmorgan <github@ghost.me.uk>
2019-02-21 15:07:01 +0000

    Added base LargeValue unit test class

---
+++ src/test/java/uk/gov/ons/validation/test/LargeValueTest.java
@@ -0,0 +1,23 @@
+package uk.gov.ons.validation.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LargeValueTest {
+
+    @Test
+    void givenCondition_getStatisticalVariableFormula_describeOutcome() {
+        assert(false);
+    }
+
+    @Test
+    void givenCondition_getValueFormula_describeOutcome() {
+        assert(false);
+    }
+
+    @Test
+    void givenCondition_run_describeOutcome() {
+        assert(false);
+    }
+}
