7b0f9c7bea68ad9f113320387600b83c6e392e73
dllmorgan <github@ghost.me.uk>
2019-02-21 18:03:10 +0000

    Added base test class for Value Change

---
+++ src/test/java/uk/gov/ons/validation/test/RuleValueChangeTest.java
@@ -0,0 +1,13 @@
+package uk.gov.ons.validation.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RuleValueChangeTest {
+
+    @Test
+    void givenIncreaseInValues_run_returnsTrue() {
+        assert(false);
+    }
+}
